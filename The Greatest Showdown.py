b65f7b80c3b6af5afc0f1bf0e7bcff55a8a7d693
Elima777 <elimazholdubaeva@gmail.com>
2024-04-26 12:37:56 -0500

    Update

--- The Greatest Showdown.py
+++ The Greatest Showdown.py
@@ -3,7 +3,6 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 num3 = int(input("Enter the third number: "))
 
-
 # Task 1: Identifying the Greatest
 max_num = max(num1, num2, num3)
 print("The largest number is:", max_num)
@@ -15,7 +14,7 @@ print("The smallest number is:", min_num)
 # Task 3: Equality Check
 if num1 == num2 == num3:
     print("All numbers are equal.")
-elif num3 != num1 or num2 != num1 or num2 != num3:
-    print("Numbers are not equal.")
-else:
+elif num1 == num2 or num1 == num3 or num2 == num3:
     print("Two numbers are equal.")
+else:
+    print("Numbers are not equal.")
